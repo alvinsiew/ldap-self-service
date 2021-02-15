a26f20864adf94b3dd22c6766d8b761513b2def1
Alvin Siew <alvin_siew@tech.gov.sg>
2021-02-15 21:04:00 +0800

    restructure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ldap-self-service
+
+## Self-compile
+
+```bash
+# MacOS
+env GOOS=darwin GOARCH=amd64 go build -o bin/ldapss cmd/ldapss/main.go
+
+# Linux
+env GOOS=linux GOARCH=amd64 go build -o bin/ldapss cmd/ldapss/main.go
+```
---
+++ internal/yamlcustom/yamlcustom.go
@@ -0,0 +1,44 @@
+package yamlcustom
+
+import (
+	"io/ioutil"
+	"log"
+	"os/user"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+// ConfigLDAP struct for yaml LDAP config
+type ConfigLDAP struct {
+	UserDN string `yaml:"userdn"`
+}
+
+// Config struct for mnc config
+type Config struct {
+	Conf []ConfigLDAP `yaml:"conf"`
+}
+
+// ParseYAML parse yaml config file
+func ParseYAML() Config {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filename, _ := filepath.Abs(usr.HomeDir + "../../config.yml")
+	yamlFile, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	return config
+}
